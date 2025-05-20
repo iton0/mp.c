421df2989f1b4b610416dc17b44327e80c78e73b
iton0 <iton224@protonmail.com>
2025-05-20 14:00:05 -0400

    move main mpc functionality to mpc.c

---
+++ src/mpc.h
@@ -0,0 +1,10 @@
+// Copyright (c) 2025 iton0. All rights reserved.
+// Licensed under the MIT license.
+// See LICENSE file in the project root for full license information.
+
+#ifndef MPC_H_
+#define MPC_H_
+
+void run_mpc(void);
+
+#endif
