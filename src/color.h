0fa3f17444769da8c8d424ea8bbf581a16c81c54
iton0 <iton224@protonmail.com>
2025-05-20 16:41:47 -0400

    add license to color.h

--- src/color.h
+++ src/color.h
@@ -1,3 +1,7 @@
+// Copyright (c) 2025 iton0. All rights reserved.
+// Licensed under the MIT license.
+// See LICENSE file in the project root for full license information.
+
 #ifndef COLOR_H_
 #define COLOR_H_
 
